2068d9095c8b976aecc6ee9ea22512d4e5c9ad4a
Nyfaria <kinaria3324@gmail.com>
2021-05-24 13:11:25 -0500

    CoFH Compat

---
+++ src/main/java/com/nyfaria/nyfsquiver/mixin/CoFHMixin.java
@@ -0,0 +1,51 @@
+package com.nyfaria.nyfsquiver.mixin;
+
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ArrowItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import cofh.lib.util.helpers.ArcheryHelper;
+import top.theillusivec4.curios.api.CuriosApi;
+
+import static cofh.lib.capability.CapabilityArchery.AMMO_ITEM_CAPABILITY;
+
+@Mixin(ArcheryHelper.class)
+public class CoFHMixin 
+{
+
+	@Shadow
+    public static boolean isArrow(ItemStack stack) {
+		return false;
+	}
+	
+	
+	@Overwrite
+    public static ItemStack findAmmo(PlayerEntity shooter) {
+
+        ItemStack offHand = shooter.getMainHandItem();
+        ItemStack mainHand = shooter.getOffhandItem();
+		ItemStack stack = CuriosApi.getCuriosHelper().findEquippedCurio(item -> item.getItem() instanceof ArrowItem,shooter)
+				.map(stringIntegerItemStackImmutableTriple -> stringIntegerItemStackImmutableTriple.right).orElse(ItemStack.EMPTY);
+
+		if(!stack.isEmpty()) {
+			return stack;
+		}
+		
+        if (offHand.getCapability(AMMO_ITEM_CAPABILITY).map(cap -> !cap.isEmpty(shooter)).orElse(false) || isArrow(offHand)) {
+            return offHand;
+        }
+        if (mainHand.getCapability(AMMO_ITEM_CAPABILITY).map(cap -> !cap.isEmpty(shooter)).orElse(false) || isArrow(mainHand)) {
+            return mainHand;
+        }
+        for (ItemStack slot : shooter.inventory.items) {
+            if (slot.getCapability(AMMO_ITEM_CAPABILITY).map(cap -> !cap.isEmpty(shooter)).orElse(false) || isArrow(slot)) {
+                return slot;
+            }
+        }
+        return ItemStack.EMPTY;
+    }
+}
